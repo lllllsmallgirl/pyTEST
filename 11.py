4b05d1b17b1fe807c167f8e902aecb4eaceab6ec
钟铭 <1257877964@qq.com>
2021-07-02 21:59:25 +0800

    delete

--- 11.py
+++
@@ -1,7 +0,0 @@
-try:
-   a=input('输入一个整数a')
-   b=input('输入一个整数b')
-   print(a/b)
-except:
-    print('zmm')
-print('dyy')
--- stusystem.py
+++ stusystem.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 filename='student.txt'
 def main():
